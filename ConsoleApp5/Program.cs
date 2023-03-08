e5da70a1748ea5732aa68f4327106468845e5e23
Felipe20041 <104681395+Felipe20041@users.noreply.github.com>
2023-03-08 14:43:12 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp3;
+
+Console.WriteLine("Hello, World!");
+
+Pessoa p1 = new Pessoa(1, "Felipe", "9999-9999");
+Console.WriteLine(p1.getNome() + " " + p1.getTelefone());
